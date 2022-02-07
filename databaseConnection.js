57bb56d8207b29dff65e6845de4f1c22ccd91e64
elisa-he <97122439+elisa-he@users.noreply.github.com>
2022-02-07 11:27:34 -0800

    database2

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
+	host: "qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "udo9opnzprghafjd",
+	password: "khdze0ky58d8a9df",
+	database: "bzq4scu6rpham0sn",
 	multipleStatements: false, 
 	reconnect: true
 };
