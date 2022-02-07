8aaaaa84cae8178f60c3555949606e3fb0625fe2
elisa-he <97122439+elisa-he@users.noreply.github.com>
2022-02-06 23:39:57 -0800

    jaws added

--- databaseConnection.js
+++ databaseConnection.js
@@ -7,15 +7,17 @@ const dbConfigHeroku = {
 	user: "b1ab7fb2ee03bc",
 	password: "2a484a2d",
 	database: "heroku_3d208ad4bd6f421",
-	multipleStatements: false
+	multipleStatements: false, 
+	reconnect: true
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "lab_example",
-	multipleStatements: false
+	host: "qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "udo9opnzprghafjd",
+	password: "khdze0ky58d8a9df",
+	database: "bzq4scu6rpham0sn",
+	multipleStatements: false,
+	reconnect:true
 };
 
 if (is_heroku) {
